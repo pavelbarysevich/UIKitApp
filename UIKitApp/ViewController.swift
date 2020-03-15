77caf2e55de336da3c5696e157b16e66d7d7186d
Pavel Barysevich <pavelbarysevich@gmail.com>
2020-03-15 18:04:01 +0300

    Initial Commit

---
+++ UIKitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitApp
+//
+//  Created by Павел Борисевич on 3/15/20.
+//  Copyright © 2020 Павел Борисевич. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
